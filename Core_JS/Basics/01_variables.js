66b701566a2cb0c7a176c59ed19fa0884f9d9cfb
Nihal Machhi <nihalmachhi11@gmail.com>
2025-09-29 18:32:45 +0530

    first commit

---
+++ Core_JS/Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1998566
+let accountEmail = "nihal@gmail.com"
+var accountPassword = "Nihal123"
+accountCity = "vadodara"
+let accountState
+
+accountEmail = "nm@gmail.com"
+accountPassword = "8526459"
+accountCity = "Bengaluru"
+
+console.log(accountId)
